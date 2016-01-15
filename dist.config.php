b6945b88cd4cb65957146de8c3983f9676173ce1
Milan Rukavina <milan@horisen.com>
2016-01-15 13:36:19 +0100

    dist config added

--- dist.config.php
+++ dist.config.php
@@ -9,6 +9,7 @@ return array(
     ),
     'client'    => array(
         'log_file'  => 'data/log/client.log',
+        'test_url'  => 'http://127.0.0.1/work/sms-bulk-mock/public//bulk-server.php?type=text&user=testuser&password=testpass&sender=Bulk+Test&receiver=%2B4178123456&dcs=GSM&text=This+is+test+message+from-cli&dlr-mask=19'
     ),
     'bulk_server' =>array(
         'port' => 8080
