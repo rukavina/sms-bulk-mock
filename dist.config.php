a765e7dc8ce3a7e9367ce09b0bc89360852da1f0
Milan Rukavina <milan@horisen.com>
2016-04-26 13:21:36 +0200

    negative dlr

--- dist.config.php
+++ dist.config.php
@@ -12,7 +12,9 @@ return array(
         'test_url'  => 'http://127.0.0.1/work/sms-bulk-mock/public//bulk-server.php?type=text&user=testuser&password=testpass&sender=Bulk+Test&receiver=%2B4178123456&dcs=GSM&text=This+is+test+message+from-cli&dlr-mask=19'
     ),
     'bulk_server' =>array(
-        'port' => 8080
+        'port' => 8080,
+        'dlr_event' => 1,
+        //'dlr_error_code' => 22 //set it you want to test negative dlr reply        
     ),    
 );
 
--- public/bulk-server.php
+++ public/bulk-server.php
@@ -64,15 +64,67 @@ $dlrUrl = $_REQUEST['dlr-url'];
 
 $log->info("Sending DRL to url: [$dlrUrl]");
 
+$errorMap = [
+    0 => 'No error',
+    1 => 'Unknown subscriber',
+    9 => 'Illegal subscriber',
+    11 => 'Teleservice not provisioned',
+    13 => 'Call barred',
+    15 => 'CUG reject',
+    19 => 'No SMS support in MS',
+    20 => 'Error in MS',
+    21 => 'Facility not supported',
+    22 => 'Memory capacity exceeded',
+    29 => 'Absent subscriber',
+    30 => 'MS busy for MT SMS',
+    36 => 'Network/Protocol failure',
+    44 => 'Illegal equipment',
+    60 => 'No paging response',
+    61 => 'GMSC congestion',
+    63 => 'HLR timeout',
+    64 => 'MSC/SGSN_timeout',
+    70 => 'SMRSE/TCP error',
+    72 => 'MT congestion',
+    75 => 'GPRS suspended',
+    80 => 'No paging response via MSC',
+    81 => 'IMSI detached',
+    82 => 'Roaming restriction',
+    83 => 'Deregistered in HLR for GSM',
+    84 => 'Purged for GSM',
+    85 => 'No paging response via SGSN',
+    86 => 'GPRS detached',
+    87 => 'Deregistered in HLR for GPRS',
+    88 => 'The MS purged for GPRS',
+    89 => 'Unidentified subscriber via MSC',
+    90 => 'Unidentified subscriber via SGSN',
+    112 => 'Originator missing credit on prepaid account',
+    113 => 'Destination missing credit on prepaid account',
+    114 => 'Error in prepaid system',
+    500 => 'Other error',
+    990 => 'HLR failure',
+    991 => 'Rejected by message text filter',
+    992 => 'Ported numbers not supported on destination',
+    993 => 'Blacklisted sender',
+    994 => 'No credit',
+    995 => 'Undeliverable',
+    996 => 'Validity expired',
+    997 => 'Blacklisted receiver',
+    998 => 'No route',
+    999 => 'Repeated submission (possible looping)',    
+];
+
+$errorCode = isset($config['bulk_server']['dlr_error_code'])? $config['bulk_server']['dlr_error_code']: 0;
+$errorDesc = isset($errorMap[$errorCode])? $errorMap[$errorCode]: '';
+
 $httpClient = new HttpClient();
 
 $dlrVars = array(
     '%U' => $messageId,	//Message ID	Message ID as returned when message is sent, see here
-    '%d' => 1,	//DLR Event, see here	1
+    '%d' => isset($config['bulk_server']['dlr_event'])? $config['bulk_server']['dlr_event']: 1, //DLR Event, see here	1
     '%s' => $_REQUEST['sender'],	//Sender	
     '%r' => $_REQUEST['receiver'],	//Receiver	
-    '%e' => '',	//Error code	26
-    '%E' => '',	//Error description	Unknown subscriber
+    '%e' => $errorCode,	//Error code	26
+    '%E' => $errorDesc,	//Error description	Unknown subscriber
     '%A' => $_REQUEST['user'],	//Account name used for submission.	YOUR_USERNAME
     '%p' => 0,	//Part number [0 to total_parts-1]	1
     '%P' => $smsParts,	//Total number of parts	3
